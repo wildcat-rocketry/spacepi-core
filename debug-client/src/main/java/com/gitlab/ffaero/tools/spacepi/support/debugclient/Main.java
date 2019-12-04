b56e8dbfc30ba203298beb3a8a9f30c5c18ece7e
Zach Deibert <zachdeibert@gmail.com>
2019-12-03 18:41:21 -0600

    Added debug-client skeleton

---
+++ debug-client/src/main/java/com/gitlab/ffaero/tools/spacepi/support/debugclient/Main.java
@@ -0,0 +1,7 @@
+package com.gitlab.ffaero.tools.spacepi.support.debugclient;
+
+abstract class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
