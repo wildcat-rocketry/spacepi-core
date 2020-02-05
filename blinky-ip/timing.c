dc3f0626e5062c830fe38771a51fe109289d4947
Zach Deibert <zachdeibert@gmail.com>
2020-02-05 13:25:08 -0600

    Wrote the encoder part

--- blinky-ip/timing.c
+++ blinky-ip/timing.c
@@ -13,6 +13,21 @@ const struct timespec delay_between_prints = {
     .tv_nsec = 0
 };
 
+const struct timespec encoding_one_high = {
+    .tv_sec = 0,
+    .tv_nsec = 800000000
+};
+
+const struct timespec encoding_zero_high = {
+    .tv_sec = 0,
+    .tv_nsec = 200000000
+};
+
+const struct timespec encoding_period = {
+    .tv_sec = 1,
+    .tv_nsec = 0
+};
+
 int timer_init(struct timespec *timer) {
     CHECK_ERROR(clock_gettime, CLOCK_REALTIME, timer);
     return 0;
