b760b5a10d175efec2531c9c5907a451807753d8
Zach Deibert <zachdeibert@gmail.com>
2020-02-05 13:25:08 -0600

    Wrote the encoder part

--- blinky-ip/timing.h
+++ blinky-ip/timing.h
@@ -5,8 +5,10 @@
 #include <stdint.h>
 
 extern const struct timespec delay_between_scans;
-
 extern const struct timespec delay_between_prints;
+extern const struct timespec encoding_one_high;
+extern const struct timespec encoding_zero_high;
+extern const struct timespec encoding_period;
 
 int timer_init(struct timespec *timer);
 int sleep_to(struct timespec *timer, struct timespec delay);
