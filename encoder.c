aaebe2e8035b67eb4562e9ae0f6555b114d347e3
Zach Deibert <zachdeibert@gmail.com>
2020-02-05 13:25:08 -0600

    Wrote the encoder part

--- encoder.c
+++ encoder.c
@@ -3,8 +3,27 @@
 #include <spacepi.h>
 #include "encoder.h"
 #include "led.h"
+#include "timing.h"
+
+int encode_bit(int bit, struct timespec *timer);
 
 int encode(uint32_t ipaddr, struct timespec *timer) {
     printf("%d.%d.%d.%d\n", (ipaddr >> 24) & 0xFF, (ipaddr >> 16) & 0xFF, (ipaddr >> 8) & 0xFF, (ipaddr >> 0) & 0xFF);
+    for (uint32_t mask = (1 << 31); mask; mask >>= 1) {
+        CHECK_ERROR(encode_bit, ipaddr & mask, timer);
+    }
+    return 0;
+}
+
+int encode_bit(int bit, struct timespec *timer) {
+    struct timespec high_timer = *timer;
+    CHECK_ERROR(set_led, 1);
+    if (bit) {
+        CHECK_ERROR(sleep_to, &high_timer, encoding_one_high);
+    } else {
+        CHECK_ERROR(sleep_to, &high_timer, encoding_zero_high);
+    }
+    CHECK_ERROR(set_led, 0);
+    CHECK_ERROR(sleep_to, timer, encoding_period);
     return 0;
 }
--- led.c
+++ led.c
@@ -3,9 +3,10 @@
 #include "led.h"
 
 int led_init(void) {
-
+    return 0;
 }
 
 int set_led(int val) {
-
+    printf("%d\n", val);
+    return 0;
 }
