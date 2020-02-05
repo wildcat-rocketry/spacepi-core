f7ab111f34099cf7f6f78e362cf65373d930a694
Zach Deibert <zachdeibert@gmail.com>
2020-02-05 13:01:16 -0600

    Made it loop

--- blinky-ip/main.c
+++ blinky-ip/main.c
@@ -2,8 +2,10 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include <spacepi.h>
 #include "ips.h"
+#include "timing.h"
 
 int main_loop(void);
 void exit_handler(int sig);
@@ -15,10 +17,31 @@ int main(int argc, const char **argv) {
 }
 
 int main_loop(void) {
-    CHECK_ERROR(ips_reset);
-    uint32_t addr;
-    while (ips_next(&addr)) {
-        printf("%d.%d.%d.%d\n", (addr >> 24) & 0xFF, (addr >> 16) & 0xFF, (addr >> 8) & 0xFF, (addr >> 0) & 0xFF);
+    struct timespec last_time;
+    struct timespec now;
+    CHECK_ERROR(clock_gettime, CLOCK_REALTIME, &last_time);
+    while (TRUE) {
+        CHECK_ERROR(clock_gettime, CLOCK_REALTIME, &now);
+        if (last_time.tv_nsec + BETWEEN_SCANS_NS < last_time.tv_nsec || last_time.tv_nsec + BETWEEN_SCANS_NS >= 1000000000L) {
+            last_time.tv_nsec += BETWEEN_SCANS_NS - 1000000000L;
+            ++last_time.tv_sec;
+        } else {
+            last_time.tv_nsec += BETWEEN_SCANS_NS;
+        }
+        last_time.tv_sec += BETWEEN_SCANS_S;
+        if (last_time.tv_nsec - now.tv_nsec < 0 || last_time.tv_nsec - now.tv_nsec > last_time.tv_nsec) {
+            now.tv_nsec = last_time.tv_nsec + 1000000000L - now.tv_nsec;
+            now.tv_sec = last_time.tv_sec - 1 - now.tv_sec;
+        } else {
+            now.tv_nsec = last_time.tv_nsec - now.tv_nsec;
+            now.tv_sec = last_time.tv_sec - now.tv_sec;
+        }
+        CHECK_ERROR(nanosleep, &now, NULL);
+        CHECK_ERROR(ips_reset);
+        uint32_t addr;
+        while (ips_next(&addr)) {
+            printf("%d.%d.%d.%d\n", (addr >> 24) & 0xFF, (addr >> 16) & 0xFF, (addr >> 8) & 0xFF, (addr >> 0) & 0xFF);
+        }
     }
 }
 
