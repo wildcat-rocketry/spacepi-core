d66e4d46c7401d048e43b478673bdd01487c774d
Zach Deibert <zachdeibert@gmail.com>
2020-02-24 11:32:39 -0600

    Moved timing code into SpacePi library

--- blinky-ip/timing.c
+++ blinky-ip/timing.c
@@ -1,6 +1,4 @@
-#include <stdint.h>
 #include <time.h>
-#include <spacepi.h>
 #include "timing.h"
 
 const struct timespec delay_between_scans = {
@@ -27,29 +25,3 @@ const struct timespec encoding_period = {
     .tv_sec = 1,
     .tv_nsec = 0
 };
-
-int timer_init(struct timespec *timer) {
-    CHECK_ERROR(clock_gettime, CLOCK_REALTIME, timer);
-    return 0;
-}
-
-int sleep_to(struct timespec *timer, struct timespec delay) {
-    struct timespec now;
-    CHECK_ERROR(timer_init, &now);
-    if (timer->tv_nsec + delay.tv_nsec < timer->tv_nsec || timer->tv_nsec + delay.tv_nsec >= 1000000000L) {
-        timer->tv_nsec += delay.tv_nsec - 1000000000L;
-        ++timer->tv_sec;
-    } else {
-        timer->tv_nsec += delay.tv_nsec;
-    }
-    timer->tv_sec += delay.tv_sec;
-    if (timer->tv_nsec - now.tv_nsec < 0 || timer->tv_nsec - now.tv_nsec > timer->tv_nsec) {
-        now.tv_nsec = timer->tv_nsec + 1000000000L - now.tv_nsec;
-        now.tv_sec = timer->tv_sec - 1 - now.tv_sec;
-    } else {
-        now.tv_nsec = timer->tv_nsec - now.tv_nsec;
-        now.tv_sec = timer->tv_sec - now.tv_sec;
-    }
-    CHECK_ERROR(nanosleep, &now, NULL);
-    return 0;
-}
--- blinky-ip/timing.h
+++ blinky-ip/timing.h
@@ -2,15 +2,11 @@
 #define TIMING_H
 
 #include <time.h>
-#include <stdint.h>
 
 extern const struct timespec delay_between_scans;
 extern const struct timespec delay_between_prints;
 extern const struct timespec encoding_one_high;
 extern const struct timespec encoding_zero_high;
 extern const struct timespec encoding_period;
-
-int timer_init(struct timespec *timer);
-int sleep_to(struct timespec *timer, struct timespec delay);
 
 #endif
