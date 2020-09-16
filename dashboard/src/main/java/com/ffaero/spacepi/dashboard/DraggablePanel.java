65742cd13bb258e2e54ee04b647351b853d8929f
Hanavan Kuhn <hanavan99@gmail.com>
2020-09-15 19:14:04 -0500

    Finished implementation of dragging/resizing

--- dashboard/src/main/java/com/ffaero/spacepi/dashboard/DraggablePanel.java
+++ dashboard/src/main/java/com/ffaero/spacepi/dashboard/DraggablePanel.java
@@ -1,15 +1,18 @@
 package com.ffaero.spacepi.dashboard;
 
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Cursor;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.GridLayout;
 import java.awt.Panel;
 import java.awt.Point;
 import java.awt.RenderingHints;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
+import java.util.function.Supplier;
 
 public class DraggablePanel extends Panel {
 
@@ -27,12 +30,29 @@ public class DraggablePanel extends Panel {
 	private static final int RESIZE_NW = 7;
 	private static final int DRAG = 8;
 
-	private IDrawable child;
+	private Panel childPanel;
+	private Supplier<Point> mousePos;
 	private Point origMousePos = null;
 	private int dragMode = 0;
+	private int origX, origY, origWidth, origHeight;
 
-	public DraggablePanel(IDrawable child) {
-		this.child = child;
+	/**
+	 * Creates a new <code>DraggablePanel</code> with the given Component as its .
+	 * Components can be added to this panel so that they can be moved around.
+	 * 
+	 * @param mousePos a supplier that returns the current position of the mouse
+	 *                 relative to the parent component
+	 */
+	public DraggablePanel(Supplier<Point> mousePos) {
+		this.mousePos = mousePos;
+
+		GridLayout layout = new GridLayout(1, 1);
+		// layout.
+		// java.awt.
+
+		childPanel = new Panel();
+		super.add(childPanel);
+
 		addMouseListener(new MouseListener() {
 
 			public void mouseClicked(MouseEvent e) {
@@ -41,7 +61,38 @@ public class DraggablePanel extends Panel {
 			}
 
 			public void mousePressed(MouseEvent e) {
-				origMousePos = e.getPoint();
+				int width = getWidth();
+				int height = getHeight();
+				Point mpos = e.getPoint();
+				origMousePos = mousePos.get();
+				origX = getX();
+				origY = getY();
+				origWidth = getWidth();
+				origHeight = getHeight();
+
+				if (mpos.x < DRAG_BOUNDS) {
+					if (mpos.y < DRAG_BOUNDS) {
+						dragMode = RESIZE_NW;
+					} else if (mpos.y > height - DRAG_BOUNDS) {
+						dragMode = RESIZE_SW;
+					} else {
+						dragMode = RESIZE_W;
+					}
+				} else if (mpos.x > width - DRAG_BOUNDS) {
+					if (mpos.y < DRAG_BOUNDS) {
+						dragMode = RESIZE_NE;
+					} else if (mpos.y > height - DRAG_BOUNDS) {
+						dragMode = RESIZE_SE;
+					} else {
+						dragMode = RESIZE_E;
+					}
+				} else if (mpos.y < DRAG_BOUNDS) {
+					dragMode = RESIZE_N;
+				} else if (mpos.y > height - DRAG_BOUNDS) {
+					dragMode = RESIZE_S;
+				} else {
+					dragMode = DRAG;
+				}
 			}
 
 			public void mouseReleased(MouseEvent e) {
@@ -62,7 +113,26 @@ public class DraggablePanel extends Panel {
 		addMouseMotionListener(new MouseMotionListener() {
 
 			public void mouseDragged(MouseEvent e) {
-
+				Point mpos = mousePos.get();
+				switch (dragMode) {
+				case RESIZE_N:
+					setLocation(origX, origY + (mpos.y - origMousePos.y));
+					setSize(origWidth, origHeight - (mpos.y - origMousePos.y));
+					break;
+				case RESIZE_E:
+					setSize(origWidth + (mpos.x - origMousePos.x), origHeight);
+					break;
+				case RESIZE_S:
+					setSize(origWidth, origHeight + (mpos.y - origMousePos.y));
+					break;
+				case RESIZE_W:
+					setLocation(origX + (mpos.x - origMousePos.x), origY);
+					setSize(origWidth - (mpos.x - origMousePos.x), origHeight);
+					break;
+				case DRAG:
+					setLocation(origX + (mpos.x - origMousePos.x), origY + (mpos.y - origMousePos.y));
+					break;
+				}
 			}
 
 			public void mouseMoved(MouseEvent e) {
@@ -99,6 +169,16 @@ public class DraggablePanel extends Panel {
 	}
 
 	@Override
+	public Component add(Component comp) {
+		return childPanel.add(comp);
+	}
+
+	@Override
+	public void remove(Component comp) {
+		childPanel.remove(comp);
+	}
+
+	@Override
 	public void paint(Graphics g) {
 		Graphics2D g2d = (Graphics2D) g.create();
 		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
@@ -108,9 +188,7 @@ public class DraggablePanel extends Panel {
 
 		g2d.setColor(Color.BLACK);
 		g2d.drawRect(0, 0, width - 1, height - 1);
-
-		Graphics2D childg = (Graphics2D) g2d.create(5, 5, width - 10, height - 10);
-		child.draw(childg, width - 10, height - 10);
+		super.paint(g2d);
 	}
 
 }
