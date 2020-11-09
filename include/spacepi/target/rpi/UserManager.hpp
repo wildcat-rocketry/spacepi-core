cab57ec8e48f10a0cc3b599418349a73745ae977
Grant Nichol <gwnichol.m@gmail.com>
2020-11-09 17:01:58 -0600

    Added Init changes for 11-09 Target

--- include/spacepi/target/rpi/UserManager.hpp
+++ include/spacepi/target/rpi/UserManager.hpp
@@ -5,24 +5,39 @@
 #include <vector>
 #include <boost/program_options.hpp>
 #include <spacepi/util/CommandConfigurable.hpp>
+#include <pwd.h>
+#include <grp.h>
+#include <shadow.h>
+
+#include <spacepi/target/rpi/User>
+#include <spacepi/target/rpi/Person>
 
 namespace spacepi {
     namespace target {
         namespace rpi {
             class UserManager {
                 public:
-                    UserNanager();
-                    void AddOrUpdateUser(std::string uname,
-                                         boost::optional<std::string> name, boost::optional<std::string> email,
-                                         boost::optional<std::string> shell, boost::optional<std::string> keys);
+                    UserManager(boost::property_tree::ptree & users);
+
+                    // Iterate through user entries and build People List
+                    void MakePeopleList(boost::property_tree::ptree & users);
+
+                    // Make a list of the system users
+                    void MakeUserList();
+
+                    bool needs_update();
+
+                    void write_users();
 
                 protected:
-                    void options(boost::program_options::options_description &desc) const;
-                    void configure(const boost::program_options::parsed_options &opts);
+                    bool update;
 
                 private:
                     std::list<spacepi::target::rpi::Users> system_users;
                     std::list<spacepi::target::rpi::People> human_users;
+
+                    std::list<uid_t> uids;
+                    uid_t next_uid(uid_t start);
             };
         }
     }
