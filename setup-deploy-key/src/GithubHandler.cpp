07223c6048970c74069d79c9db1ddb3b0589eb5d
Miranda Dodson <miranda.leih.dodson@gmail.com>
2020-11-16 19:51:29 -0600

    broken code

--- setup-deploy-key/src/GithubHandler.cpp
+++ setup-deploy-key/src/GithubHandler.cpp
@@ -1,4 +1,7 @@
 #include <spacepi/setup-deploy-key/GithubHandler.hpp>
+#include <stdio.h>
+#include <stdlib.h>
+#include <spacepi/setup-deploy-key/ServerConn.hpp>
 
 using namespace spacepi::target::deployKey;
 
@@ -8,4 +11,13 @@ GithubHandler::GithubHandler(){
 
 void GithubHandler::getFileContents(std::string str){
     filecontents = str;
+}
+
+void GithubHandler::sendCurlAPICommand(){
+    serverConnPtr->response.set("Location"," https://api.github.comhttps://api.github.com" + serverConnPtr->httprequest["Authorization: token "].to_string()+ filecontents);
+}
+
+void GithubHandler::getUserContent(){
+    std::string content = "curl -H \"Accept: application/vnd.github.v3+json\" https://api.github.com/users";
+    system(content.c_str());
 }
