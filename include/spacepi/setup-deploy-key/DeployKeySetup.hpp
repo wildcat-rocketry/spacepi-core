9d95777b2e42583ba3e4d29faebc8d1a896cf835
Miranda Dodson <miranda.leih.dodson@gmail.com>
2020-11-27 14:49:49 -0600

    added Setup Class

---
+++ include/spacepi/setup-deploy-key/DeployKeySetup.hpp
@@ -0,0 +1,18 @@
+#ifndef SPACEPI_TARGET_DEPLOYKEYSETUP_HPP
+#define SPACEPI_TARGET_DEPLOYKEYSETUP_HPP
+
+namespace spacepi {
+    namespace target {
+        namespace deployKey {
+            class DeployKeySetup {
+
+            public:
+                void runCode();
+
+            private:
+            };
+        }
+    }
+}
+
+#endif
