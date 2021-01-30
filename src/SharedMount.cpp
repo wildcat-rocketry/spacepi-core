6b9a71205d34d99ae9e550baf56e3e1ee138a51e
Miranda Dodson <miranda.leih.dodson@gmail.com>
2021-01-30 00:27:28 -0600

    mounting/unmounting drives

---
+++ src/SharedMount.cpp
@@ -0,0 +1,28 @@
+#include <memory>
+#include <spacepi/liblinux/SharedMount.hpp>
+#include <spacepi/liblinux/UniqueMount.hpp>
+#include <string>
+#include <utility>
+
+using namespace spacepi::liblinux;
+
+SharedMount::SharedMount(const std::string &blockDevice, const std::string &mountPoint, const std::string &options, const std::string &type){
+    unique.reset(new UniqueMount(blockDevice,mountPoint,options,type));
+}
+
+SharedMount::SharedMount(UniqueMount &&mount){
+    unique.reset(new UniqueMount(std::move(mount)));
+}
+
+bool SharedMount::isMounted() const noexcept{
+    bool mounted = unique->isMounted();
+    return mounted;
+}
+
+void SharedMount::mount(){
+    unique->mount();
+}
+
+void SharedMount::unmount(){
+    unique->unmount();
+}
