2c69ccae116a9e8d2389da5724d6627b16e8b6a5
Miranda Dodson <miranda.leih.dodson@gmail.com>
2021-01-30 16:33:39 -0600

    fixed stuff

--- src/OSBuilder.cpp
+++ src/OSBuilder.cpp
@@ -1,11 +1,16 @@
 #include <fstream>
 #include <iostream>
 #include <SpacePi.hpp>
+#include <spacepi/liblinux/Partition.hpp>
+#include <spacepi/liblinux/PartitionTable.hpp>
+#include <spacepi/liblinux/SharedMount.hpp>
+#include <spacepi/liblinux/UniqueMount.hpp>
 #include <spacepi/target/OSBuilder.hpp>
 
 using namespace std;
 using namespace spacepi::util;
 using namespace spacepi::target;
+using namespace spacepi::liblinux;
 
 OSBuilder::OSBuilder(Command &cmd) : CommandConfigurable("OS Builder", cmd) {
     fromCommand(configFile, "config-file", "XML file path to configure the OS from");
@@ -21,4 +26,17 @@ void OSBuilder::runCommand() {
          << "Output File: " << outFile << endl
          << "   Data Dir: " << dataDir << endl;
     cout << "File written to " << outFile << endl;
+
+    PartitionTable partTable;
+    partTable.addPartition(Partition() .setUUID("thing0") .setMountPoint("/") .setFSType("ext4") .setOptions("defaults,sync,noatime,ro") .setSize("256M") .setType("0C"));
+    partTable.addPartition(Partition() .setUUID("thing1") .setMountPoint("/boot") .setFSType("vfat") .setOptions("defaults,sync,noatime,ro") .setSize("768M") .setType("0F"));
+    partTable.printFstab(cout);
+    cout << std::endl;
+    partTable.setLabel("dos");
+    partTable.setUnit("sectors");
+    partTable.printSfdisk(cout);
+
+    cout << "Starting Mounting Test....." << std::endl;
+    SharedMount sharedMount("/home","/tmp/home","bind,defaults,ro","none");
+    sleep(60);
 }
