f762de788727e871ffd9cf951797c7e8876d21eb
Zach Deibert <zachdeibert@gmail.com>
2021-01-29 21:08:26 -0600

    Wrote mounting class headers

---
+++ include/spacepi/liblinux/SharedMount.hpp
@@ -0,0 +1,25 @@
+#ifndef SPACEPI_TARGETLIB_LINUX_SHAREDMOUNT_HPP
+#define SPACEPI_TARGETLIB_LINUX_SHAREDMOUNT_HPP
+
+#include <memory>
+#include <string>
+#include <spacepi/liblinux/UniqueMount.hpp>
+
+namespace spacepi {
+    namespace liblinux {
+        class SharedMount {
+            public:
+                SharedMount(const std::string &blockDevice, const std::string &mountPoint, const std::string &options, const std::string &type);
+                SharedMount(UniqueMount &&mount);
+
+                bool isMounted() const noexcept;
+                void mount();
+                void unmount();
+
+            private:
+                std::shared_ptr<UniqueMount> unique;
+        };
+    }
+}
+
+#endif
