3387fdb6da6eb1b60d78c4c56f930f5f95893616
Zach Deibert <zachdeibert@gmail.com>
2021-02-02 12:46:07 -0600

    Added headers for loop device resource classes

---
+++ include/spacepi/liblinux/UniqueLoopDevice.hpp
@@ -0,0 +1,32 @@
+#ifndef SPACEPI_TARGETLIB_LINUX_UNIQUELOOPDEVICE_HPP
+#define SPACEPI_TARGETLIB_LINUX_UNIQUELOOPDEVICE_HPP
+
+#include <string>
+
+namespace spacepi {
+    namespace liblinux {
+        class UniqueLoopDevice {
+            public:
+                UniqueLoopDevice(const std::string &imageFile);
+                ~UniqueLoopDevice();
+
+                UniqueLoopDevice(UniqueLoopDevice &) = delete;
+                UniqueLoopDevice &operator =(UniqueLoopDevice &) = delete;
+
+                UniqueLoopDevice(UniqueLoopDevice &&move) noexcept;
+                UniqueLoopDevice &operator =(UniqueLoopDevice &&move) noexcept;
+
+                std::string getBlockDevice(int partNo) const;
+
+                bool isMounted() const noexcept;
+                void mount();
+                void unmount();
+
+            private:
+                std::string imageFile;
+                std::string baseDevice;
+        };
+    }
+}
+
+#endif
