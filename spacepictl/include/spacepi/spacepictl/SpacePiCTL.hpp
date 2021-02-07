1cffcb0ebbecd5aa4368e33612542c3f59455687
Grant Nichol <gwnichol.m@gmail.com>
2021-02-07 00:09:35 -0600

    Add spacepictl exec

--- spacepictl/include/spacepi/spacepictl/SpacePiCTL.hpp
+++ spacepictl/include/spacepi/spacepictl/SpacePiCTL.hpp
@@ -2,7 +2,9 @@
 #define SPACEPI_SPACEPICTL_SPACEPICTL_HPP
 
 #include <boost/filesystem.hpp>
+#include <boost/property_tree/ptree.hpp>
 #include <SpacePi.hpp>
+#include <vector>
 
 namespace spacepi {
     namespace spacepictl {
@@ -10,17 +12,18 @@ namespace spacepi {
             public:
 
                 int initialize_system();
-                int userspace_utility();
+                int userspace_utility(std::vector<std::string> argv);
                 int run_reconfiguration();
 
-                int main(int argc, char ** argv);
+                int main(std::vector<std::string> argv);
 
             private:
 
                 bool points_to_file(boost::filesystem::path& p);
+                int spacepictl_exec(std::vector<std::string> argv);
+                bool get_module(std::string module_name, boost::property_tree::ptree& module_config);
 
-                int arg_count;
-                char ** arg_vector;
+                std::vector<std::string> arg_vector;
         };
     }
 }
--- spacepictl/src/SpacePiCTL.cpp
+++ spacepictl/src/SpacePiCTL.cpp
@@ -1,17 +1,20 @@
 // This is the program for initializing the spacepi
 #include <exception>
+#include <vector>
 
 #include <SpacePi.hpp>
 #include <spacepi/spacepictl/UserManager.hpp>
 #include <spacepi/spacepictl/System.hpp>
 #include <spacepi/spacepictl/SpacePiCTL.hpp>
 #include <spacepi/spacepictl/FSTransaction.hpp>
+
 #include <unistd.h> 
 #include <sys/mount.h>
 
 #include <boost/process.hpp>
 #include <boost/property_tree/xml_parser.hpp>
 #include <boost/filesystem.hpp>
+#include <boost/foreach.hpp>
 
 #define SPACEPI_ETC "/etc/spacepi"
 #define FIRSTTIME_SETUP_PATH "/usr/local/sbin/install-spacepi-boardsupport"
@@ -29,27 +32,24 @@ namespace fs = boost::filesystem;
 namespace bp = boost::process;
 
 // Main
-int main(int argc, char ** argv){
+int main(int argc, char* argv[]){
     SpacePiCTL control;
     Logger log("spacepictl");
     try{
-        control.main(argc, argv);
+        control.main(vector<string>(&argv[0], &argv[argc]));
     } catch (const Exception &ex){
         log(LogLevel::Error) << "Error running spacepictl: " << ex.what() << "\n" << ex.getPointer();
     }
 }
 
-int SpacePiCTL::main(int argc, char ** argv){
+int SpacePiCTL::main(vector<string> argv){
 
-    arg_count = argc;
-    arg_vector = argv;
-
-    if(getpid() == 1 || argc == 2 && strcmp(argv[1], "--force-init") == 0) {
+    if(getpid() == 1 || argv.size() == 2 && argv[1] == "--force-init") {
         // Am da boss
         return initialize_system();
     }
 
-    return userspace_utility();
+    return userspace_utility(argv);
 }
 
 int SpacePiCTL::initialize_system(){
@@ -103,8 +103,78 @@ bool SpacePiCTL::points_to_file(fs::path& p){
     }
 }
 
-int SpacePiCTL::userspace_utility(){
-    throw EXCEPTION(StateException("User utility not implemented"));
+int SpacePiCTL::userspace_utility(vector<string> argv){
+    if(argv.size() > 1){
+        try{
+            if(argv[1] == "exec"){
+                return spacepictl_exec(argv);
+            } else {
+                cerr << "No action \"" << argv[1] << "\"\n\n";
+            }
+        } catch (const Exception &ex){
+            log(LogLevel::Error) << "Error running spacepictl " << argv[1] << ": " << ex.what() << "\n" << ex.getPointer();
+        }
+
+    } else {
+        cerr << "Not enough arguments.\n\n";
+    }
+
+    cerr << "USAGE: " << argv[0] << " <action> [[options] ... ]\n";
+    cerr << "Available actions:\n";
+    cerr << "    exec     Execute a SpacePi service\n";
+    return 1;
+}
+
+bool SpacePiCTL::get_module(string module_name, ptree& module_config){
+    ptree pt;
+    read_xml(RUNNING_CONF, pt);
+
+    BOOST_FOREACH(const ptree::value_type &pair, pt.get_child("config")){
+        if(pair.first == "module"){
+            const ptree & module = pair.second;
+            if(module.get<string>("type") == module_name){
+                module_config = module;
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
+
+int SpacePiCTL::spacepictl_exec(vector<string> argv){
+    if(argv.size() == 3){
+        ptree module;
+        if(get_module(argv[2], module)){
+            cout << "Found module \"" << argv[2] << "\"\n";
+            vector<char*> args;
+            vector<string> str_args;
+            args.push_back(const_cast<char*>(argv[2].c_str()));
+            BOOST_FOREACH(const ptree::value_type &pair, module.get_child("options")){
+                str_args.push_back("--" + pair.first);
+                str_args.push_back(pair.second.get_value<string>());
+            }
+            for(const auto &arg : str_args){
+                args.push_back(const_cast<char*>(arg.c_str()));
+            }
+            args.push_back(NULL);
+
+            execvp(argv[2].c_str(), &args[0]);
+
+            stringstream ss;
+            ss << "Error executing";
+            for(auto &arg : args) ss << " " << arg;
+            ss << ": " << strerror(errno) << "!" << endl;
+            log(LogLevel::Error) << ss.str();
+            return 1;
+        } else {
+            cerr << "Module \"" << argv[2] << "\" not found\n";
+        }
+    } else {
+        cerr << "Incorrect number of arguments (" << argv.size() << "). Expects 3.\n\n";
+    }
+
+    cerr << "USAGE: " << argv[0] << " " << argv[1] << " UNIT_NAME\n";
     return 1;
 }
 
