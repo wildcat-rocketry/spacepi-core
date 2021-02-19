7cd8e10e0a0120dc4aae0d6cffc22bd7efe5cfab
Zach Deibert <zachdeibert@gmail.com>
2021-02-18 18:01:41 -0600

    Suppressed ABI warnings

--- cmake/Compat.cmake
+++ cmake/Compat.cmake
@@ -2,3 +2,7 @@ if (${CMAKE_VERSION} VERSION_LESS "3.16")
     function (target_precompile_headers)
     endfunction()
 endif()
+
+if (CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+    add_compile_options(-Wno-psabi)
+endif()
