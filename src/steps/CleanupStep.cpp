21f6cdd5aafcdbb9eb8c7e668aba65edd0a5a0bd
Zach Deibert <zachdeibert@gmail.com>
2021-02-22 00:25:21 -0600

    Wrote CleanupStep

--- src/steps/CleanupStep.cpp
+++ src/steps/CleanupStep.cpp
@@ -1,11 +1,18 @@
-#include <SpacePi.hpp>
-#include <spacepi/liblinux/steps/CleanupStep.hpp>
+#include <vector>
+#include <unistd.h>
 #include <spacepi/liblinux/InstallationData.hpp>
+#include <spacepi/liblinux/SharedLoopDevice.hpp>
+#include <spacepi/liblinux/SharedMount.hpp>
+#include <spacepi/liblinux/SharedTempDir.hpp>
+#include <spacepi/liblinux/steps/CleanupStep.hpp>
 
-using namespace spacepi::util;
+using namespace std;
 using namespace spacepi::liblinux;
 using namespace spacepi::liblinux::steps;
 
 void CleanupStep::run(InstallationData &data) {
-    throw EXCEPTION(StateException("Not implemented"));
+    sync();
+    data.deleteData<vector<SharedMount>>();
+    data.deleteData<SharedLoopDevice>();
+    data.deleteData<SharedTempDir>();
 }
