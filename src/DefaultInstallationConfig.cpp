e9a85b9f38c8eda74202420e89860ba4b94ac990
Zach Deibert <zachdeibert@gmail.com>
2021-02-25 18:54:58 -0600

    Updated base package list

--- src/DefaultInstallationConfig.cpp
+++ src/DefaultInstallationConfig.cpp
@@ -4,7 +4,17 @@ using namespace spacepi::liblinux;
 
 DefaultInstallationConfig::DefaultInstallationConfig() noexcept {
     sourceDir = "/usr/local/src/spacepi";
-    packages = "cmake,build-essential,protobuf-compiler,libboost-all-dev,automake";
+    packages =
+        ",automake"
+        ",build-essential"
+        ",cmake"
+        ",libboost-all-dev"
+        ",libgpiod-dev"
+        ",libi2c-dev"
+        ",libprotobuf-dev"
+        ",libssl-dev"
+        ",protobuf-compiler"
+            + 1;
     components = "main,contrib,non-free";
     osRelease = "stable";
     aptURL = "http://deb.debian.org/debian";
