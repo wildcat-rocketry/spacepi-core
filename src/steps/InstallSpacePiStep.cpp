19d8264954d578bada48dcd47c429102ef367b97
Zach Deibert <zachdeibert@gmail.com>
2021-02-25 18:54:56 -0600

    Wrote InstallSpacePiStep

--- src/steps/InstallSpacePiStep.cpp
+++ src/steps/InstallSpacePiStep.cpp
@@ -1,11 +1,27 @@
+#include <string>
+#include <boost/filesystem.hpp>
 #include <SpacePi.hpp>
 #include <spacepi/liblinux/steps/InstallSpacePiStep.hpp>
 #include <spacepi/liblinux/InstallationData.hpp>
+#include <spacepi/liblinux/InstallationConfig.hpp>
+#include <spacepi/liblinux/SharedTempDir.hpp>
+#include <spacepi/liblinux/UniqueChroot.hpp>
+#include <spacepi/liblinux/UniqueProcess.hpp>
 
+using namespace std;
+using namespace boost::filesystem;
 using namespace spacepi::util;
 using namespace spacepi::liblinux;
 using namespace spacepi::liblinux::steps;
 
 void InstallSpacePiStep::run(InstallationData &data) {
-    throw EXCEPTION(StateException("Not implemented"));
+    string rootDir = data.getData<SharedTempDir>().getPath();
+    InstallationConfig &config = data.getData<InstallationConfig>();
+    UniqueChroot chroot(rootDir);
+    path buildDir = config.sourceDir / "build";
+    UniqueProcess make(false, false, false, "/usr/bin/env", { "make", "-C", buildDir.native(), "install" });
+    make.wait();
+    if (make.getExitCode() != 0) {
+        throw EXCEPTION(ResourceException("Unable to install SpacePi"));
+    }
 }
