94797c417154ef5fe6dbf721efa6071f6571217f
Zach Deibert <zachdeibert@gmail.com>
2021-03-01 10:02:01 -0600

    Added setting spacepictl config

--- src/steps/InstallSpacePiStep.cpp
+++ src/steps/InstallSpacePiStep.cpp
@@ -2,8 +2,10 @@
 #include <boost/filesystem.hpp>
 #include <SpacePi.hpp>
 #include <spacepi/liblinux/steps/InstallSpacePiStep.hpp>
+#include <spacepi/liblinux/Config.hpp>
 #include <spacepi/liblinux/InstallationData.hpp>
 #include <spacepi/liblinux/InstallationConfig.hpp>
+#include <spacepi/liblinux/InstallationOptions.hpp>
 #include <spacepi/liblinux/SharedTempDir.hpp>
 #include <spacepi/liblinux/UniqueChroot.hpp>
 #include <spacepi/liblinux/UniqueProcess.hpp>
@@ -17,11 +19,22 @@ using namespace spacepi::liblinux::steps;
 void InstallSpacePiStep::run(InstallationData &data) {
     string rootDir = data.getData<SharedTempDir>().getPath();
     InstallationConfig &config = data.getData<InstallationConfig>();
+    path configFile = canonical(relative(data.getData<InstallationOptions>().getConfigFile(), CMAKE_SOURCE_DIR), CMAKE_SOURCE_DIR);
     UniqueChroot chroot(rootDir);
     path buildDir = config.sourceDir / "build";
     UniqueProcess make(false, false, false, "/usr/bin/env", { "make", "-C", buildDir.native(), "install" });
     make.wait();
     if (make.getExitCode() != 0) {
         throw EXCEPTION(ResourceException("Unable to install SpacePi"));
+    }
+    UniqueProcess ldconfig(false, false, false, "/sbin/ldconfig", {});
+    ldconfig.wait();
+    if (ldconfig.getExitCode() != 0) {
+        throw EXCEPTION(ResourceException("Unable to set library include path"));
+    }
+    UniqueProcess spacepictl(false, false, false, "/usr/local/bin/spacepictl", { "config-set", configFile.native() });
+    spacepictl.wait();
+    if (spacepictl.getExitCode() != 0) {
+        throw EXCEPTION(ResourceException("Unable to set SpacePi package config"));
     }
 }
