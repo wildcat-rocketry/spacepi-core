60efbe0a90a535437a54c58e6674a5a7ba2d3416
Zach Deibert <zachdeibert@gmail.com>
2021-03-01 11:02:02 -0600

    Added sysfs and procfs to chroot

--- src/steps/MountPartitionsStep.cpp
+++ src/steps/MountPartitionsStep.cpp
@@ -1,3 +1,5 @@
+#include <string>
+#include <boost/filesystem.hpp>
 #include <SpacePi.hpp>
 #include <vector>
 #include <spacepi/liblinux/steps/MountPartitionsStep.hpp>
@@ -6,15 +8,23 @@
 #include <spacepi/liblinux/Image.hpp>
 #include <spacepi/liblinux/SharedMount.hpp>
 
+using namespace std;
+using namespace boost::filesystem;
 using namespace spacepi::util;
 using namespace spacepi::liblinux;
 using namespace spacepi::liblinux::steps;
 
 void MountPartitionsStep::run(InstallationData &data) {
     data.initData<SharedTempDir>("mount");
-    SharedTempDir &root = data.getData<SharedTempDir>();
+    string root = data.getData<SharedTempDir>().getPath();
     Image &image = data.getData<Image>();
     PartitionTable &pt = data.getData<PartitionTable>();
-    std::vector<SharedMount> mounts = image.mountPartitions(pt, root.getPath(), true);
+    std::vector<SharedMount> mounts = image.mountPartitions(pt, root, true);
+    path procfs = root / "proc";
+    create_directories(procfs);
+    mounts.emplace_back("none", procfs.native(), "nosuid,nodev,noexec,noatime", "proc");
+    path sysfs = root / "sys";
+    create_directories(sysfs);
+    mounts.emplace_back("none", sysfs.native(), "nosuid,nodev,noexec,noatime", "sysfs");
     data.initData<std::vector<SharedMount>>(mounts);
 }
