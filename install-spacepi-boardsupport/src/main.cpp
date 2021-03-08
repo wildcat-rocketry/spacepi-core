2ec5663a258a983b31915c5c340fd0ba29fcf37b
Zach Deibert <zachdeibert@gmail.com>
2021-03-08 16:38:25 -0600

    Removed install-spacepi-boardsupport

--- install-spacepi-boardsupport/src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <SpacePi.hpp>
-
-using namespace spacepi::log;
-
-int main(int argc, const char **argv) {
-    Logger log("install-spacepi-boardsupport");
-    log(LogLevel::Info) << "Hello, world!";
-    return 0;
-}
