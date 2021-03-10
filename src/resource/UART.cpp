cea272f00332986b2cd774e99ad06b5929a3a29c
Zach Deibert <zachdeibert@gmail.com>
2021-03-10 14:17:50 -0600

    Split up mock resource implementations

--- src/resource/UART.cpp
+++ src/resource/UART.cpp
@@ -1,91 +1,12 @@
-#include <iomanip>
-#include <ios>
 #include <istream>
 #include <memory>
 #include <ostream>
-#include <streambuf>
 #include <string>
-#include <spacepi/log/AutoLog.hpp>
-#include <spacepi/log/LogLevel.hpp>
-#include <spacepi/log/LogStream.hpp>
 #include <spacepi/resource/ResourceFactory.hpp>
 #include <spacepi/resource/UART.hpp>
 
-namespace spacepi {
-    namespace resource {
-        namespace detail {
-            class MockUART : public UART, private spacepi::log::AutoLog<decltype("core:resource:mock:uart"_autolog)> {
-                public:
-                    MockUART(const std::string &name) noexcept;
-
-                    int getBAUDRate() const noexcept;
-                    void setBAUDRate(int baud);
-
-                protected:
-                    std::streamsize xsgetn(char *buffer, std::streamsize count);
-                    std::streamsize xsputn(char *buffer, std::streamsize count);
-
-                private:
-                    std::string name;
-                    int baud;
-            };
-
-            class MockUARTFactory : public ResourceFactory<UART> {
-                public:
-                    static MockUARTFactory instance;
-
-                    MockUARTFactory();
-
-                private:
-                    std::shared_ptr<UART> create(const std::string &name);
-            };
-        }
-    }
-}
-
 using namespace std;
-using namespace spacepi::log;
 using namespace spacepi::resource;
-using namespace spacepi::resource::detail;
-
-MockUARTFactory MockUARTFactory::instance;
-
-MockUART::MockUART(const string &name) noexcept : name(name), baud(115200) {
-    log(LogLevel::Debug) << "Created new UART '" << name << "'.";
-}
-
-int MockUART::getBAUDRate() const noexcept {
-    return baud;
-}
-
-void MockUART::setBAUDRate(int baud) {
-    log(LogLevel::Debug) << "Setting UART '" << name << "' BAUD rate to " << baud << ".";
-    this->baud = baud;
-}
-
-streamsize MockUART::xsgetn(char *buffer, streamsize count) {
-    log(LogLevel::Debug) << "Reading UART '" << name << "': " << count << " bytes";
-    return count;
-}
-
-streamsize MockUART::xsputn(char *buffer, streamsize count) {
-    LogStream os = log(LogLevel::Debug);
-    os << "Writing UART '" << name << "': " << count << "bytes:" << hex << setfill('0');
-    for (int i = 0; i < count; ++i) {
-        if (i % 8 == 0) {
-            os << "\n   ";
-        }
-        os << " 0x" << setw(2) << (int) buffer[i] << setw(0);
-    }
-    return count;
-}
-
-MockUARTFactory::MockUARTFactory() : ResourceFactory<UART>("mock") {
-}
-
-shared_ptr<UART> MockUARTFactory::create(const string &name) {
-    return shared_ptr<UART>(new MockUART(name));
-}
 
 UART::UART() : iostream(this) {
 }
---
+++ src/resource/detail/MockI2C.cpp
@@ -0,0 +1,169 @@
+#include <cstdint>
+#include <iomanip>
+#include <ios>
+#include <memory>
+#include <ostream>
+#include <string>
+#include <utility>
+#include <vector>
+#include <spacepi/log/Logger.hpp>
+#include <spacepi/log/LogLevel.hpp>
+#include <spacepi/log/LogStream.hpp>
+#include <spacepi/resource/I2C.hpp>
+#include <spacepi/resource/ResourceFactory.hpp>
+
+namespace spacepi {
+    namespace resource {
+        namespace detail {
+            class MockI2C : public I2C {
+                public:
+                    MockI2C(const std::string &name) noexcept;
+
+                    int getSpeed() const noexcept;
+                    void setSpeed(int speed);
+
+                    bool isSMBusErrorCheckingEnabled() const noexcept;
+                    void enableSMBusErrorChecking();
+                    void disableSMBusErrorChecking();
+
+                    void quickWriteSMBus(bool val);
+                    uint8_t readByteSMBus();
+                    void writeByteSMBus(uint8_t data);
+                    uint8_t readByteSMBus(uint8_t command);
+                    void writeByteSMBus(uint8_t command, uint8_t data);
+                    uint16_t readWordSMBus(uint8_t command);
+                    void writeWordSMBus(uint8_t command, uint16_t data);
+                    uint16_t processCallSMBus(uint8_t command, uint16_t data);
+                    void readBlockSMBus(uint8_t command, uint8_t *data);
+                    void writeBlockSMBus(uint8_t command, uint8_t *data, uint8_t length);
+
+                protected:
+                    void doTransaction(const std::vector<std::pair<uint8_t *, int16_t>> &steps);
+
+                private:
+                    spacepi::log::Logger log;
+                    std::string name;
+                    int speed;
+                    bool errorChecking;
+            };
+
+            class MockI2CFactory : public ResourceFactory<I2C> {
+                public:
+                    static MockI2CFactory instance;
+
+                    MockI2CFactory();
+
+                private:
+                    std::shared_ptr<I2C> create(const std::string &name);
+            };
+        }
+    }
+}
+
+using namespace std;
+using namespace spacepi::log;
+using namespace spacepi::resource;
+using namespace spacepi::resource::detail;
+
+MockI2CFactory MockI2CFactory::instance;
+
+MockI2C::MockI2C(const string &name) noexcept : log("core:resource:mock:i2c"), name(name), speed(100000), errorChecking(false) {
+    log(LogLevel::Debug) << "Created new I2C '" << name << "'.";
+}
+
+int MockI2C::getSpeed() const noexcept {
+    return speed;
+}
+
+void MockI2C::setSpeed(int speed) {
+    log(LogLevel::Debug) << "Setting I2C '" << name << "' speed to " << speed << ".";
+    this->speed = speed;
+}
+
+bool MockI2C::isSMBusErrorCheckingEnabled() const noexcept {
+    return errorChecking;
+}
+
+void MockI2C::enableSMBusErrorChecking() {
+    log(LogLevel::Debug) << "Enabling error checking for SMBus '" << name << "'.";
+    errorChecking = true;
+}
+
+void MockI2C::disableSMBusErrorChecking() {
+    log(LogLevel::Debug) << "Disabling error checking for SMBus '" << name << "'.";
+    errorChecking = false;
+}
+
+void MockI2C::quickWriteSMBus(bool val) {
+    log(LogLevel::Debug) << "Quick write to SMBus '" << name << "': " << val << ".";
+}
+
+uint8_t MockI2C::readByteSMBus() {
+    log(LogLevel::Debug) << "Byte read from SMBus '" << name << "'.";
+    return 0xFF;
+}
+
+void MockI2C::writeByteSMBus(uint8_t data) {
+    log(LogLevel::Debug) << "Byte write to SMBus '" << name << "': 0x" << hex << setw(2) << data << setw(0) << ".";
+}
+
+uint8_t MockI2C::readByteSMBus(uint8_t command) {
+    log(LogLevel::Debug) << "Byte read (command 0x" << hex << setw(2) << command << setw(0) << ") from SMBus '" << name << "'.";
+    return 0xFF;
+}
+
+void MockI2C::writeByteSMBus(uint8_t command, uint8_t data) {
+    log(LogLevel::Debug) << "Byte write (command 0x" << hex << setw(2) << command << setw(0) << ") to SMBus '" << name << "': 0x" << setw(2) << data << setw(0) << ".";
+}
+
+uint16_t MockI2C::readWordSMBus(uint8_t command) {
+    log(LogLevel::Debug) << "Word read (command 0x" << hex << setw(2) << command << setw(0) << ") from SMBus '" << name << "'.";
+    return 0xFFFF;
+}
+
+void MockI2C::writeWordSMBus(uint8_t command, uint16_t data) {
+    log(LogLevel::Debug) << "Word write (command 0x" << hex << setw(2) << command << setw(0) << ") to SMBus '" << name << "': 0x" << setw(4) << data << setw(0) << ".";
+}
+
+uint16_t MockI2C::processCallSMBus(uint8_t command, uint16_t data) {
+    log(LogLevel::Debug) << "Process call (command 0x" << hex << setw(2) << command << setw(0) << ") to SMBus '" << name << "': 0x" << setw(4) << data << setw(0) << ".";
+    return 0xFFFF;
+}
+
+void MockI2C::readBlockSMBus(uint8_t command, uint8_t *data) {
+    log(LogLevel::Debug) << "Block read (command 0x" << hex << setw(2) << command << setw(0) << ") from SMBus '" << name << "'.";
+}
+
+void MockI2C::writeBlockSMBus(uint8_t command, uint8_t *data, uint8_t length) {
+    LogStream os = log(LogLevel::Debug);
+    os << "Block write (command 0x" << hex << setw(2) << command << setw(0) << " to SMBUs '" << name << "':";
+    for (int i = 0; i < length; ++i) {
+        if (i % 8 == 0) {
+            os << "\n   ";
+        }
+        os << " 0x" << setw(2) << (int) data[i] << setw(0);
+    }
+}
+
+void MockI2C::doTransaction(const vector<pair<uint8_t *, int16_t>> &steps) {
+    LogStream os = log(LogLevel::Debug);
+    os << "I2C transaction:";
+    for (vector<pair<uint8_t *, int16_t>>::const_iterator it = steps.begin(); it != steps.end(); ++it) {
+        if (it->second < 0) {
+            os << "\n    Write of " << -it->second << " bytes:" << hex << setfill('0');
+            for (int i = 0; i < -it->second; ++i) {
+                os << " 0x" << setw(2) << (int) it->first[i];
+            }
+            os << setw(0) << dec << ".";
+        } else {
+            os << "\n    Read of " << it->second << " bytes.";
+        }
+    }
+}
+
+MockI2CFactory::MockI2CFactory() : ResourceFactory<I2C>("mock") {
+}
+
+shared_ptr<I2C> MockI2CFactory::create(const string &name) {
+    return shared_ptr<I2C>(new MockI2C(name));
+}
