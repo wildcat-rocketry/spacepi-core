dd76c9ade040540f63fb67718b78f4cd2beb03a8
Zach Deibert <zachdeibert@gmail.com>
2021-03-13 17:16:30 -0600

    Added spacepictl remote-id

---
+++ spacepictl/src/RemoteID.cpp
@@ -0,0 +1,46 @@
+#include <exception>
+#include <iostream>
+#include <ostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <sys/types.h>
+#include <unistd.h>
+#include <SpacePi.hpp>
+#include <spacepi/spacepictl/RemoteID.hpp>
+#include <spacepi/spacepictl/Verb.hpp>
+
+using namespace std;
+using namespace std::chrono;
+using namespace spacepi::concurrent;
+using namespace spacepi::spacepictl;
+
+RemoteID RemoteID::instance;
+
+RemoteID::RemoteID() noexcept : Verb("remote-id", "Prints out the current UID and GID, then blocks forever") {
+}
+
+bool RemoteID::run(const vector<string> &args) {
+    if (!args.empty()) {
+        printErrorHelp("remote-id verb does not take any arguments.");
+        return false;
+    }
+
+    ostringstream ss;
+    ss << "\n"
+          "spacepi-targetlib-linux:UID=" << getuid() << "\n"
+          "spacepi-targetlib-linux:GID=" << getgid() << "\n";
+    cerr << ss.str();
+
+    try {
+        while (true) {
+            Sleep::duration(nanoseconds::max());
+        }
+    } catch (const exception &) {
+    }
+    return true;
+}
+
+ostream &RemoteID::printHelp(ostream &os) const noexcept {
+    return os << "\n";
+}
