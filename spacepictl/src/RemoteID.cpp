46dc2ed188f6723e5580ce2779012b6b59622673
Zach Deibert <zachdeibert@gmail.com>
2021-03-23 16:17:00 -0500

    Fixed NFS connections to read-only root

--- spacepictl/src/RemoteID.cpp
+++ spacepictl/src/RemoteID.cpp
@@ -6,8 +6,10 @@
 #include <string>
 #include <vector>
 #include <linux/prctl.h>
+#include <sys/mount.h>
 #include <sys/prctl.h>
 #include <sys/types.h>
+#include <sys/vfs.h>
 #include <pwd.h>
 #include <unistd.h>
 #include <SpacePi.hpp>
@@ -16,6 +18,7 @@
 
 using namespace std;
 using namespace spacepi::concurrent;
+using namespace spacepi::log;
 using namespace spacepi::spacepictl;
 
 RemoteID RemoteID::instance;
@@ -30,6 +33,13 @@ bool RemoteID::run(const vector<string> &args) {
     }
 
     prctl(PR_SET_PDEATHSIG, SIGHUP);
+
+    struct statfs stat;
+    if (statfs("/", &stat) < 0) {
+        log(LogLevel::Warning) << "Unable to stat /: " << strerror(errno);
+    } else if (mount(nullptr, "/", nullptr, MS_REMOUNT | (stat.f_flags & ~MS_RDONLY), nullptr) < 0) {
+        log(LogLevel::Warning) << "Unable to remount / as read-write: " << strerror(errno);
+    }
 
     ostringstream ss;
     ss << "\n"
