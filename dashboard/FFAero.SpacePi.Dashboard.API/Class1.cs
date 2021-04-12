62bf2924e27a71dbf225a688d57b5d2e48a84fa1
Zach Deibert <zachdeibert@gmail.com>
2021-04-12 15:21:46 -0500

    Created dashboard projects

---
+++ dashboard/FFAero.SpacePi.Dashboard.API/Class1.cs
@@ -0,0 +1,6 @@
+﻿using System;
+
+namespace FFAero.SpacePi.Dashboard.API {
+    public class Class1 {
+    }
+}
---
+++ dashboard/FFAero.SpacePi.Dashboard.Core/Class1.cs
@@ -0,0 +1,6 @@
+﻿using System;
+
+namespace FFAero.SpacePi.Dashboard.Core {
+    public class Class1 {
+    }
+}
