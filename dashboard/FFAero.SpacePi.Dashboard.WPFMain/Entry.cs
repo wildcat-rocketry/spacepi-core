90bdf4f72967935e23602e79cf19312ea84f819f
Zach Deibert <zachdeibert@gmail.com>
2021-04-23 20:29:00 -0500

    Started preparing project for automatic configuration with CMake

---
+++ dashboard/FFAero.SpacePi.Dashboard.WPFMain/Entry.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FFAero.SpacePi.Dashboard.WPF;
+
+namespace FFAero.SpacePi.Dashboard.WPFMain {
+    static class Entry {
+        [STAThread]
+        public static void Main() {
+            App app = new();
+            app.InitializeComponent();
+            app.Run();
+        }
+    }
+}
