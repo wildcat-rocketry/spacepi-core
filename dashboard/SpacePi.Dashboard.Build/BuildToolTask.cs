10eb8a52b9431212597bceed4d5fca776e890338
Zach Deibert <zachdeibert@gmail.com>
2021-04-26 15:14:40 -0500

    Changed root namespace

---
+++ dashboard/SpacePi.Dashboard.Build/BuildToolTask.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace SpacePi.Dashboard.Build {
+    public abstract class BuildToolTask : ToolTask {
+        protected override string ToolName => "SpacePi.Dashboard.BuildTool";
+
+        protected override string GenerateFullPathToTool() {
+            return Path.GetFullPath(Path.Combine(Assembly.GetExecutingAssembly().Location, "..", "..", "..", "..", "SpacePi.Dashboard.BuildTool", "bin", "net6.0", "SpacePi.Dashboard.BuildTool.exe"));
+        }
+
+        protected override abstract string GenerateCommandLineCommands();
+
+        protected override MessageImportance StandardOutputLoggingImportance => MessageImportance.Normal;
+
+        protected override MessageImportance StandardErrorLoggingImportance => MessageImportance.High;
+    }
+}
---
+++ dashboard/SpacePi.Dashboard.WPFMain/Entry.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SpacePi.Dashboard.Analyzer;
+using SpacePi.Dashboard.WPF;
+
+namespace SpacePi.Dashboard.WPFMain {
+    static class Entry {
+        [STAThread]
+        public static void Main() {
+            PluginContext.Setup();
+            App app = new();
+            app.InitializeComponent();
+            app.Run();
+        }
+    }
+}
