0d9e614d3e0e60e51102f7627b1fa059162a66d5
Zach Deibert <zachdeibert@gmail.com>
2021-06-07 00:16:46 -0500

    Made it compile protobufs into the C# build

---
+++ dashboard/SpacePi.Dashboard.Analyzer/Diagnostics.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.CodeAnalysis;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpacePi.Dashboard.Analyzer {
+    static class Diagnostics {
+        public static DiagnosticDescriptor ProtocBuildFailed = new(
+            "SPI0001",
+            "Protoc plugin build failed",
+            $"Failed to build {BuildConfig.protoc_gen_spacepi_csharp.TARGET_NAME_IF_EXISTS} target using CMake",
+            "SpacePi",
+            DiagnosticSeverity.Error,
+            true);
+        public static DiagnosticDescriptor ProtocFailure = new(
+            "SPI0002",
+            "Protoc failure",
+            "Protoc reported a build failure",
+            "SpacePi",
+            DiagnosticSeverity.Error,
+            true);
+    }
+}
