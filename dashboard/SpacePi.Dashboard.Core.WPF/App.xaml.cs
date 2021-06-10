37091c0085265d5365c0db454f9545cafde97b60
Zach Deibert <zachdeibert@gmail.com>
2021-06-10 12:45:37 -0500

    Refactored WPF projects

--- dashboard/SpacePi.Dashboard.Core.WPF/App.xaml.cs
+++ dashboard/SpacePi.Dashboard.Core.WPF/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace SpacePi.Dashboard.WPF {
+namespace SpacePi.Dashboard.Core.WPF {
     public partial class App : Application {
     }
 }
--- dashboard/SpacePi.Dashboard.WPF/Entry.cs
+++ dashboard/SpacePi.Dashboard.WPF/Entry.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using SpacePi.Dashboard.Analyzer;
-using SpacePi.Dashboard.WPF;
+using SpacePi.Dashboard.Core.WPF;
 
-namespace SpacePi.Dashboard.WPFMain {
+namespace SpacePi.Dashboard.WPF {
     static class Entry {
         [STAThread]
         public static void Main() {
