67f14131341a2b1250da2734eac7de93da1f4da6
Zach Deibert <zachdeibert@gmail.com>
2021-06-10 11:37:32 -0500

    Removed weird API dependencies

--- cmake/SpacePiDashboard.cmake
+++ cmake/SpacePiDashboard.cmake
@@ -3,11 +3,10 @@
 #     [NOT_PLUGIN]
 #     [TYPE <guid>]
 #     [PROJ_TYPE <tproj>]
-#     [DEPENDS <guid> [<guid> ...]]
 #     [CONFIG_FILE <file> [<file> ...]]
 # )
 function (spacepi_dashboard_plugin SPACEPI_DASHBOARD_PROJ SPACEPI_DASHBOARD_GUID)
-    cmake_parse_arguments(SPACEPI_DASHBOARD "NOT_PLUGIN" "TYPE;PROJ_TYPE" "DEPENDS;CONFIG_FILE" ${ARGN})
+    cmake_parse_arguments(SPACEPI_DASHBOARD "NOT_PLUGIN" "TYPE;PROJ_TYPE" "CONFIG_FILE" ${ARGN})
 
     if (NOT SPACEPI_DASHBOARD_TYPE)
         set(SPACEPI_DASHBOARD_TYPE "{9A19103F-16F7-4668-BE54-9A1E7A4F7556}")
@@ -15,10 +14,6 @@ function (spacepi_dashboard_plugin SPACEPI_DASHBOARD_PROJ SPACEPI_DASHBOARD_GUID
 
     if (NOT SPACEPI_DASHBOARD_PROJ_TYPE)
         set(SPACEPI_DASHBOARD_PROJ_TYPE "csproj")
-    endif()
-
-    if (NOT SPACEPI_DASHBOARD_NOT_PLUGIN)
-        list(APPEND SPACEPI_DASHBOARD_DEPENDS "{0F502909-B2ED-419D-96A1-DFA6D482C94D}")
     endif()
 
     get_target_property(slnProjs dashboard-metadata SPACEPI_DASHBOARD_SLN_PROJS)
@@ -36,26 +31,10 @@ function (spacepi_dashboard_plugin SPACEPI_DASHBOARD_PROJ SPACEPI_DASHBOARD_GUID
         file(GENERATE OUTPUT "${CMAKE_BINARY_DIR}/_dashboard/${SPACEPI_DASHBOARD_PROJ}/${file}" INPUT "${CMAKE_BINARY_DIR}/_dashboard/${SPACEPI_DASHBOARD_PROJ}/${file}.in")
     endforeach()
 
-    if (SPACEPI_DASHBOARD_DEPENDS)
-        string(APPEND slnProjs
-"Project(\"${SPACEPI_DASHBOARD_TYPE}\") = \"${SPACEPI_DASHBOARD_PROJ}\", \"${relProjBuild}\", \"${SPACEPI_DASHBOARD_GUID}\"
-	ProjectSection(ProjectDependencies) = postProject
-")
-        foreach (dep IN LISTS SPACEPI_DASHBOARD_DEPENDS)
-            string(APPEND slnProjs
-"		${dep} = ${dep}
-")
-        endforeach()
-        string(APPEND slnProjs
-"	EndProjectSection
-EndProject
-")
-    else()
-        string(APPEND slnProjs
+    string(APPEND slnProjs
 "Project(\"${SPACEPI_DASHBOARD_TYPE}\") = \"${SPACEPI_DASHBOARD_PROJ}\", \"${relProjBuild}\", \"${SPACEPI_DASHBOARD_GUID}\"
 EndProject
 ")
-    endif()
     string(APPEND slnCfgs
 "		${SPACEPI_DASHBOARD_GUID}.Debug|Any CPU.ActiveCfg = Debug|Any CPU
 		${SPACEPI_DASHBOARD_GUID}.Debug|Any CPU.Build.0 = Debug|Any CPU
