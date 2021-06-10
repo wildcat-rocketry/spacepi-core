976bef4ba3458851d6c1892d911024bb4fd73723
Zach Deibert <zachdeibert@gmail.com>
2021-06-10 11:48:54 -0500

    Removed SpacePi.Dashboard.BuildConfig project

--- dashboard/SpacePi.Dashboard.Analyzer/ProtobufGenerator.cs
+++ dashboard/SpacePi.Dashboard.Analyzer/ProtobufGenerator.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis;
+using SpacePi.Dashboard.API;
 
 namespace SpacePi.Dashboard.Analyzer {
     [Generator]
