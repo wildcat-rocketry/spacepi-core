a88436b075ae57951b31c641689d162bb6cfe731
Zach Deibert <zachdeibert@gmail.com>
2021-06-11 12:45:07 -0500

    Extracted common functionality into CorePlugin

---
+++ dashboard/SpacePi.Dashboard.Core.WPF/CorePlugin.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpacePi.Dashboard.Core.WPF {
+    public abstract class CorePlugin : Core.CorePlugin {
+        public override string Name => $"{nameof(SpacePi)}.{nameof(Dashboard)}.{nameof(Core)}.{nameof(WPF)}.{PluginName}";
+
+        static CorePlugin() {
+            if (AssemblyVersion != GetAssemblyVersion()) {
+                throw new NotSupportedException($"{nameof(Core)} <-> {nameof(Core)}.{nameof(WPF)} version mismatch");
+            }
+        }
+    }
+}
---
+++ dashboard/SpacePi.Dashboard.Core/CorePlugin.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using SpacePi.Dashboard.API;
+
+namespace SpacePi.Dashboard.Core {
+    public abstract class CorePlugin : IPlugin {
+        protected static readonly Version AssemblyVersion = GetAssemblyVersion();
+
+        protected abstract string PluginName { get; }
+
+        public virtual string Name => $"{nameof(SpacePi)}.{nameof(Dashboard)}.{nameof(Core)}.{PluginName}";
+
+        public Version Version => AssemblyVersion;
+
+        protected static Version GetAssemblyVersion() => Assembly.GetCallingAssembly().GetName().Version;
+    }
+}
