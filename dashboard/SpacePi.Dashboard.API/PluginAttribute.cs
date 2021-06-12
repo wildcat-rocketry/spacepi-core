0c022857cb4ce3f358d81caae6701b9cea7fbcc6
Zach Deibert <zachdeibert@gmail.com>
2021-06-12 00:14:07 -0500

    Made plugins have a load priority

--- dashboard/SpacePi.Dashboard.API/PluginAttribute.cs
+++ dashboard/SpacePi.Dashboard.API/PluginAttribute.cs
@@ -7,5 +7,8 @@ using System.Threading.Tasks;
 namespace SpacePi.Dashboard.API {
     [AttributeUsage(AttributeTargets.Class)]
     public class PluginAttribute : Attribute {
+        public readonly int Priority;
+
+        public PluginAttribute(int priority = 0) => Priority = priority;
     }
 }
