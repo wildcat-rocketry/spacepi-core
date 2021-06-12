288ca050e6ce255fd5c945125d4ac4b9db20e201
Zach Deibert <zachdeibert@gmail.com>
2021-06-12 00:58:47 -0500

    Fixed writing log files on every source generator run

--- dashboard/SpacePi.Dashboard.Analyzer/DebugSourceGenerator.cs
+++ dashboard/SpacePi.Dashboard.Analyzer/DebugSourceGenerator.cs
@@ -19,12 +19,12 @@ namespace SpacePi.Dashboard.Analyzer {
         public void Initialize(GeneratorInitializationContext ctx) => DebugAction(nameof(Initialize), () => DebugInitialize(ctx));
 
         protected void DebugAction(string name, Action action) {
-            TextWriter stdout = Console.Out;
-            TextWriter stderr = Console.Error;
-            Directory.CreateDirectory(BaseDir);
-            using (TextWriter log = new StreamWriter(new FileStream(Path.Combine(BaseDir, $"{GetType().Name}.{name}.log"), FileMode.Create))) {
+            using StringWriter log = new();
+            try {
+                TextWriter stdout = Console.Out;
                 Console.SetOut(log);
                 try {
+                    TextWriter stderr = Console.Error;
                     Console.SetError(log);
                     try {
                         action();
@@ -35,6 +35,12 @@ namespace SpacePi.Dashboard.Analyzer {
                     }
                 } finally {
                     Console.SetOut(stdout);
+                }
+            } finally {
+                string str = log.ToString();
+                if (str.Length > 0) {
+                    Directory.CreateDirectory(BaseDir);
+                    File.WriteAllText(Path.Combine(BaseDir, $"{GetType().Name}.{name}.log"), str);
                 }
             }
         }
