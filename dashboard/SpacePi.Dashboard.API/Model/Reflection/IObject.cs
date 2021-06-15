bd6b160d82420b83c04207bd1bf0bdb68860ebb0
Zach Deibert <zachdeibert@gmail.com>
2021-06-14 20:26:31 -0500

    Added SpacePi.Dashboard.API.Model.Serialization namespace

--- dashboard/SpacePi.Dashboard.API/Model/Reflection/IObject.cs
+++ dashboard/SpacePi.Dashboard.API/Model/Reflection/IObject.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +7,5 @@ using System.Threading.Tasks;
 namespace SpacePi.Dashboard.API.Model.Reflection {
     public interface IObject {
         IClass Reflection { get; }
-
-        void Serialize(Stream s);
-
-        void Parse(Stream s);
     }
 }
