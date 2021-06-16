e1342c1e58450cc254f8e2b53973cb396414e9fb
Zach Deibert <zachdeibert@gmail.com>
2021-06-16 00:41:36 -0500

    Made analyzer model easily printable

--- dashboard/SpacePi.Dashboard.Analyzer/Plugin/Model/BoundPlugin.cs
+++ dashboard/SpacePi.Dashboard.Analyzer/Plugin/Model/BoundPlugin.cs
@@ -4,10 +4,18 @@ using System.Text;
 using Microsoft.CodeAnalysis;
 
 namespace SpacePi.Dashboard.Analyzer.Plugin.Model {
-    record BoundPlugin<TSource, TDest> {
+    record BoundPlugin<TSource, TDest> : DebuggableRecord {
         public TSource Parent;
         public string FieldName;
         public ITypeSymbol TargetClassSymbol;
         public TDest TargetClass;
+
+        protected override bool PrintMembers(StringBuilder str) {
+            str.Append($"\"{nameof(Parent)}\": \"(recursion)\", ");
+            str.Append($"\"{nameof(FieldName)}\": \"{FieldName}\", ");
+            str.Append($"\"{nameof(TargetClassSymbol)}\": \"{TargetClassSymbol}\", ");
+            str.Append($"\"{nameof(TargetClass)}\": \"(recursion)\", ");
+            return true;
+        }
     }
 }
--- dashboard/SpacePi.Dashboard.Analyzer/Plugin/Model/PluginClass.cs
+++ dashboard/SpacePi.Dashboard.Analyzer/Plugin/Model/PluginClass.cs
@@ -4,11 +4,22 @@ using System.Text;
 using Microsoft.CodeAnalysis;
 
 namespace SpacePi.Dashboard.Analyzer.Plugin.Model {
-    record PluginClass {
+    record PluginClass : DebuggableRecord {
         public string FQCN;
         public ITypeSymbol Symbol;
         public int Index;
         public ContextClass[] Contexts = Array.Empty<ContextClass>();
         public List<BoundPlugin<PluginClass, PluginClass>> BoundPlugins = new();
+
+        protected override bool PrintMembers(StringBuilder str) {
+            str.Append($"\"{nameof(FQCN)}\": \"{FQCN}\", ");
+            str.Append($"\"{nameof(Symbol)}\": \"{Symbol}\", ");
+            str.Append($"\"{nameof(Index)}\": {Index}, ");
+            str.Append($"\"{nameof(Contexts)}\": ");
+            PrintList(str, Contexts);
+            str.Append(", ");
+            str.Append($"\"{nameof(BoundPlugins)}\": ");
+            return PrintList(str, BoundPlugins);
+        }
     }
 }
