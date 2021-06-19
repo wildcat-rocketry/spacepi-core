8c05a29eafa7838431a810bd5459c4443686bc33
Zach Deibert <zachdeibert@gmail.com>
2021-06-19 00:03:34 -0500

    Added unit test project

---
+++ dashboard/SpacePi.Dashboard.Test/TestTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SpacePi.Dashboard.Test {
+    public class TestTest {
+        [Fact]
+        public void TrueIsTrue() {
+            Assert.True(true);
+        }
+    }
+}
