e601bbbc678b0f8eb3a70d7f176985334d21fb08
Zach Deibert <zachdeibert@gmail.com>
2021-06-19 00:07:11 -0500

    Added transient property to reflection API

--- dashboard/SpacePi.Dashboard.API/Model/Reflection/VectorClassField.cs
+++ dashboard/SpacePi.Dashboard.API/Model/Reflection/VectorClassField.cs
@@ -20,6 +20,8 @@ namespace SpacePi.Dashboard.API.Model.Reflection {
 
         public int Number { get; }
 
+        public bool Transient { get; }
+
         public IClass this[int idx] {
             get => List[idx];
             set => List[idx] = (Type) value;
@@ -33,9 +35,10 @@ namespace SpacePi.Dashboard.API.Model.Reflection {
 
         private void Changed(object sender, NotifyCollectionChangedEventArgs e) => CollectionChanged?.Invoke(this, e);
 
-        public VectorClassField(string name, int number, ObservableCollection<Type> list, Func<Type> ctor) {
+        public VectorClassField(string name, int number, bool transient, ObservableCollection<Type> list, Func<Type> ctor) {
             Name = name;
             Number = number;
+            Transient = transient;
             List = new ForgivingList<Type>(list);
             Ctor = ctor;
             list.CollectionChanged += Changed;
--- dashboard/SpacePi.Dashboard.Core/DeveloperTools/DeveloperToolsContext.cs
+++ dashboard/SpacePi.Dashboard.Core/DeveloperTools/DeveloperToolsContext.cs
@@ -29,14 +29,14 @@ namespace SpacePi.Dashboard.Core.DeveloperTools {
                 int enumField = 1;
                 double scalarDouble = 1.23;
                 TestClass subClassField = new("SubClassField",
-                    new ScalarEnumField("EnumField", 0, new ModelEnum("Fruit", (0, "Apple"), (1, "Banana"), (2, "Cherry")), () => enumField, v => enumField = v),
-                    new ScalarPrimitiveField("ScalarDouble", 0, IPrimitiveField.Types.Double, () => scalarDouble, v => scalarDouble = (double) v)
+                    new ScalarEnumField("EnumField", 0, false, new ModelEnum("Fruit", (0, "Apple"), (1, "Banana"), (2, "Cherry")), () => enumField, v => enumField = v),
+                    new ScalarPrimitiveField("ScalarDouble", 0, false, IPrimitiveField.Types.Double, () => scalarDouble, v => scalarDouble = (double) v)
                 );
                 TestClass model = new("TestingModelClass",
-                    new VectorPrimitiveField<int>("MyIntList", 0, IPrimitiveField.Types.Int32, myIntList),
-                    new ScalarClassField<TestClass>("SubClassField", 0, () => subClassField, v => subClassField = v)
+                    new VectorPrimitiveField<int>("MyIntList", 0, false, IPrimitiveField.Types.Int32, myIntList),
+                    new ScalarClassField<TestClass>("SubClassField", 0, false, () => subClassField, v => subClassField = v)
                 );
-                Root = new ScalarClassNode(new ScalarClassField<TestClass>("Dashboard Model", 0, () => model, _ => throw new NotSupportedException()));
+                Root = new ScalarClassNode(new ScalarClassField<TestClass>("Dashboard Model", 0, false, () => model, _ => throw new NotSupportedException()));
             }
         }
     }
