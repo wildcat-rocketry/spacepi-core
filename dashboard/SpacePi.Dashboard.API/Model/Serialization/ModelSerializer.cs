48e98b3fb5084f03f349a1de69ecbeb956254353
Grant Nichol <gwnichol@ksu.edu>
2021-06-22 21:35:40 -0500

    Add Varint encode and decode

--- dashboard/SpacePi.Dashboard.API/Model/Serialization/ModelSerializer.cs
+++ dashboard/SpacePi.Dashboard.API/Model/Serialization/ModelSerializer.cs
@@ -2,12 +2,64 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using SpacePi.Dashboard.API.Model.Reflection;
 
 namespace SpacePi.Dashboard.API.Model.Serialization {
     public static class ModelSerializer {
+        /// <summary>
+        /// Stream a UInt64 into the stream
+        /// </summary>
+        /// <param name="number"></param>
+        public static void ToBase128(UInt64 number, Stream stream)
+        {
+            List<byte> bytes = new List<byte>();
+            while (number > 0)
+            {
+                byte newValue = (byte)(number & 0x7F);
+
+                number >>= 7;
+                
+                if(number > 0)
+                {
+                    newValue |= 0x80; 
+                }
+
+                bytes.Add(newValue);
+            }
+
+            stream.Write(bytes.ToArray(), 0, bytes.Count);
+        }
+
+        public static void ToBase128(Int64 number, Stream stream)
+        {
+            ToBase128((UInt64)number, stream); // Not perfect, negative numbers can get very long
+            return;
+        }
+
+        public static UInt64 FromBase128(byte[] bytes, int maxLength = 10)
+        {
+            UInt64 newNumber = 0;
+            for(int i = 0; i < maxLength; i++)
+            {
+                byte cur_byte = bytes[i];
+                newNumber |= ((cur_byte & (UInt64)0x7F) << (i * 7));
+                if((cur_byte & 0x80) == 0)
+                {
+                    break; // That was the last one
+                }
+            }
+
+            return newNumber;
+        }
+
+        public static Int64 FromBase128Signed(byte[] bytes, int maxLength = 10)
+        {
+            return (Int64)FromBase128(bytes, maxLength);
+        }
+
         public static void Serialize(this IObject obj, Stream stream) => throw new NotImplementedException();
 
         public static void Parse(this IObject obj, Stream stream) => throw new NotImplementedException();
---
+++ dashboard/SpacePi.Dashboard.Test/TestSerializeDeserialize.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Xunit;
+
+using SpacePi.Dashboard.API.Model.Serialization;
+
+namespace SpacePi.Dashboard.Test {
+    public class TestSerializeDeserialize {
+        [Fact]
+        public void SimpleVarintEncode() {
+            using(MemoryStream memStream = new MemoryStream(4))
+            {
+                ModelSerializer.ToBase128(1, memStream);
+                Assert.Equal(1, memStream.Position);
+                memStream.Position = 0;
+                int firstByte = memStream.ReadByte();
+                Assert.True(firstByte == 0x01);
+            }
+        }
+
+        [Fact]
+        public void SlightlyMoreComplexVarintEncode() {
+            using(MemoryStream memStream = new MemoryStream(4))
+            {
+                ModelSerializer.ToBase128(300, memStream);
+                Assert.Equal(2, memStream.Position);
+                memStream.Position = 0;
+                Assert.Equal(0b10101100, memStream.ReadByte());
+                Assert.Equal(0b00000010, memStream.ReadByte());
+            }
+        }
+
+        [Fact]
+        public void MuchMoreComplexVarintEncode() {
+            using(MemoryStream memStream = new MemoryStream(8))
+            {
+                ModelSerializer.ToBase128(12345678, memStream);
+                Assert.Equal(4, memStream.Position);
+                memStream.Position = 0;
+                Assert.Equal(0b11001110, memStream.ReadByte());
+                Assert.Equal(0b11000010, memStream.ReadByte());
+                Assert.Equal(0b11110001, memStream.ReadByte());
+                Assert.Equal(0b00000101, memStream.ReadByte());
+            }
+        }
+
+        [Fact]
+        public void NegativeVarintEncode() {
+            using(MemoryStream memStream = new MemoryStream(12))
+            {
+                ModelSerializer.ToBase128(-3345, memStream);
+                Assert.Equal(10, memStream.Position);
+                memStream.Position = 0;
+                Assert.Equal(0b11101111, memStream.ReadByte());
+                Assert.Equal(0b11100101, memStream.ReadByte());
+                Assert.Equal(0b11111111, memStream.ReadByte());
+                Assert.Equal(0b11111111, memStream.ReadByte());
+                Assert.Equal(0b11111111, memStream.ReadByte());
+                Assert.Equal(0b11111111, memStream.ReadByte());
+                Assert.Equal(0b11111111, memStream.ReadByte());
+                Assert.Equal(0b11111111, memStream.ReadByte());
+                Assert.Equal(0b11111111, memStream.ReadByte());
+                Assert.Equal(0b00000001, memStream.ReadByte());
+            }
+        }
+
+        [Fact]
+        public void SimpleVarintDecode()
+        {
+            byte[] bytes = { 0b00000001 };
+            Assert.Equal(1, (long)ModelSerializer.FromBase128(bytes, 1));
+        }
+
+        [Fact]
+        public void SlightlyMoreComplexVarintDecode()
+        {
+            byte[] bytes = { 0b10101100, 0b00000010 };
+            Assert.Equal(300, (long)ModelSerializer.FromBase128(bytes, 2));
+        }
+
+        [Fact]
+        public void NegativeVarintDecode()
+        {
+            byte[] bytes = { 0b11101111, 0b11100101, 0b11111111, 0b11111111, 0b11111111, 0b11111111, 0b11111111, 0b11111111, 0b11111111, 0b00000001 };
+            Assert.Equal(-3345, ModelSerializer.FromBase128Signed(bytes, 10));
+        }
+
+
+    }
+}
