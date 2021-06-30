c07f39071fe602f8f69546dbd4a78eff4f8db1d9
Grant Nichol <gwnichol@ksu.edu>
2021-06-29 19:54:56 -0500

    Update varint

--- dashboard/SpacePi.Dashboard.Test/TestSerializeDeserialize.cs
+++ dashboard/SpacePi.Dashboard.Test/TestSerializeDeserialize.cs
@@ -12,83 +12,76 @@ namespace SpacePi.Dashboard.Test {
     public class TestSerializeDeserialize {
         [Fact]
         public void SimpleVarintEncode() {
-            using(MemoryStream memStream = new MemoryStream(4))
-            {
-                ModelSerializer.ToBase128(1, memStream);
-                Assert.Equal(1, memStream.Position);
-                memStream.Position = 0;
-                int firstByte = memStream.ReadByte();
-                Assert.True(firstByte == 0x01);
-            }
+            MemoryStream memStream = new MemoryStream(4);
+            ModelSerializer.ToBase128((ulong)1, memStream);
+            Assert.Equal(1, memStream.Position);
+            memStream.Position = 0;
+            int firstByte = memStream.ReadByte();
+            Assert.True(firstByte == 0x01);
         }
 
         [Fact]
         public void SlightlyMoreComplexVarintEncode() {
-            using(MemoryStream memStream = new MemoryStream(4))
-            {
-                ModelSerializer.ToBase128(300, memStream);
-                Assert.Equal(2, memStream.Position);
-                memStream.Position = 0;
-                Assert.Equal(0b10101100, memStream.ReadByte());
-                Assert.Equal(0b00000010, memStream.ReadByte());
-            }
+            MemoryStream memStream = new MemoryStream(4);
+            ModelSerializer.ToBase128((ulong)300, memStream);
+            Assert.Equal(2, memStream.Position);
+            memStream.Position = 0;
+            Assert.Equal(0b10101100, memStream.ReadByte());
+            Assert.Equal(0b00000010, memStream.ReadByte());
         }
 
         [Fact]
         public void MuchMoreComplexVarintEncode() {
-            using(MemoryStream memStream = new MemoryStream(8))
-            {
-                ModelSerializer.ToBase128(12345678, memStream);
-                Assert.Equal(4, memStream.Position);
-                memStream.Position = 0;
-                Assert.Equal(0b11001110, memStream.ReadByte());
-                Assert.Equal(0b11000010, memStream.ReadByte());
-                Assert.Equal(0b11110001, memStream.ReadByte());
-                Assert.Equal(0b00000101, memStream.ReadByte());
-            }
-        }
-
-        [Fact]
-        public void NegativeVarintEncode() {
-            using(MemoryStream memStream = new MemoryStream(12))
-            {
-                ModelSerializer.ToBase128(-3345, memStream);
-                Assert.Equal(10, memStream.Position);
-                memStream.Position = 0;
-                Assert.Equal(0b11101111, memStream.ReadByte());
-                Assert.Equal(0b11100101, memStream.ReadByte());
-                Assert.Equal(0b11111111, memStream.ReadByte());
-                Assert.Equal(0b11111111, memStream.ReadByte());
-                Assert.Equal(0b11111111, memStream.ReadByte());
-                Assert.Equal(0b11111111, memStream.ReadByte());
-                Assert.Equal(0b11111111, memStream.ReadByte());
-                Assert.Equal(0b11111111, memStream.ReadByte());
-                Assert.Equal(0b11111111, memStream.ReadByte());
-                Assert.Equal(0b00000001, memStream.ReadByte());
-            }
+            MemoryStream memStream = new MemoryStream(8);
+            ModelSerializer.ToBase128((ulong)12345678, memStream);
+            Assert.Equal(4, memStream.Position);
+            memStream.Position = 0;
+            Assert.Equal(0b11001110, memStream.ReadByte());
+            Assert.Equal(0b11000010, memStream.ReadByte());
+            Assert.Equal(0b11110001, memStream.ReadByte());
+            Assert.Equal(0b00000101, memStream.ReadByte());
         }
 
         [Fact]
         public void SimpleVarintDecode()
         {
-            byte[] bytes = { 0b00000001 };
-            Assert.Equal(1, (long)ModelSerializer.FromBase128(bytes, 1));
+            MemoryStream memStream = new MemoryStream(1);
+            memStream.WriteByte(0b00000001);
+            memStream.Seek(0, SeekOrigin.Begin);
+            Assert.Equal(1, (long)ModelSerializer.FromBase128(memStream));
         }
 
         [Fact]
         public void SlightlyMoreComplexVarintDecode()
         {
-            byte[] bytes = { 0b10101100, 0b00000010 };
-            Assert.Equal(300, (long)ModelSerializer.FromBase128(bytes, 2));
+            MemoryStream memStream = new MemoryStream(2);
+            memStream.WriteByte(0b10101100);
+            memStream.WriteByte(0b00000010);
+            memStream.Seek(0, SeekOrigin.Begin);
+            Assert.Equal(300, (long)ModelSerializer.FromBase128(memStream));
         }
 
         [Fact]
         public void NegativeVarintDecode()
         {
-            byte[] bytes = { 0b11101111, 0b11100101, 0b11111111, 0b11111111, 0b11111111, 0b11111111, 0b11111111, 0b11111111, 0b11111111, 0b00000001 };
-            Assert.Equal(-3345, ModelSerializer.FromBase128Signed(bytes, 10));
+            MemoryStream memStream = new MemoryStream(10);
+            memStream.WriteByte(0b00000001);
+            memStream.Seek(0, SeekOrigin.Begin);
+            Assert.Equal(-1, ModelSerializer.FromBase128Signed(memStream));
         }
 
-
+        [Theory]
+        [InlineData(-420)]
+        [InlineData(420)]
+        [InlineData(69)]
+        [InlineData(-69)]
+        [InlineData(-6942069)]
+        public void NegativeVarint(long testVal)
+        {
+            MemoryStream memStream = new MemoryStream(12);
+            ModelSerializer.ToBase128(testVal, memStream);
+            memStream.Seek(0, SeekOrigin.Begin);
+            Assert.Equal(testVal, ModelSerializer.FromBase128Signed(memStream));
+        }
     }
 }
