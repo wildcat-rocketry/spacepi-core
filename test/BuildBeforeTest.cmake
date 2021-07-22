8745d913bbe2bc1a5c33dcf50b7a428822099d5b
Zach Deibert <zachdeibert@gmail.com>
2021-07-22 01:32:11 -0500

    Added testing framework

---
+++ test/BuildBeforeTest.cmake
@@ -0,0 +1,1 @@
+execute_process(COMMAND "${CMAKE_COMMAND}" --build "${CMAKE_BINARY_DIR}" --config "$<CONFIG>" --target spacepi-test)
---
+++ test/src/test.cpp
@@ -0,0 +1,9 @@
+#include <gtest/gtest.h>
+
+TEST(test, test) {
+    ASSERT_EQ(2 + 2, 4);
+}
+
+TEST(test, test2) {
+    //ASSERT_EQ(2 + 2, 5);
+}
