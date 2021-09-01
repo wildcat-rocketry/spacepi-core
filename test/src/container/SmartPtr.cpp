e5e005233f6a994d155f28eb727e28861f54c6d8
Zach Deibert <zachdeibert@gmail.com>
2021-09-01 13:14:45 -0500

    Added 'unique' property to SmartPtrs

--- test/src/container/SmartPtr.cpp
+++ test/src/container/SmartPtr.cpp
@@ -11,14 +11,18 @@ using namespace spacepi::test;
 TEST(container_SmartPtr, Static) {
     SmartPtr<ElementType, Static> ptr = nullptr;
     EXPECT_FALSE(ptr);
+    EXPECT_TRUE(ptr.unique);
 
     ptr.reset(Construct, 42);
     EXPECT_TRUE(ptr);
+    EXPECT_TRUE(ptr.unique);
     EXPECT_EQ(ptr->getValue(), 42);
     EXPECT_EQ(ptr->getType(), ElementType::Init);
 
     SmartPtr<ElementType, Static> copy(ptr);
     EXPECT_TRUE(copy);
+    EXPECT_FALSE(copy.unique);
+    EXPECT_FALSE(ptr.unique);
     EXPECT_EQ(&*copy, (ElementType *) ptr);
 
     {
@@ -26,14 +30,19 @@ TEST(container_SmartPtr, Static) {
         EXPECT_TRUE(moved);
         EXPECT_TRUE(copy);
         EXPECT_FALSE(ptr);
+        EXPECT_FALSE(moved.unique);
+        EXPECT_FALSE(copy.unique);
+        EXPECT_TRUE(ptr.unique);
         EXPECT_EQ(&*copy, &*moved);
     }
 
     SmartPtr<ElementType, Static> dummy(Construct);
     EXPECT_TRUE(dummy);
+    EXPECT_FALSE(dummy.unique);
     EXPECT_EQ(dummy->getValue(), 0);
     EXPECT_NE(&*dummy, &*copy);
     EXPECT_TRUE(copy);
+    EXPECT_FALSE(copy.unique);
     EXPECT_EQ(copy->getValue(), 42);
     EXPECT_EQ(copy->getType(), ElementType::Move);
 }
@@ -41,14 +50,18 @@ TEST(container_SmartPtr, Static) {
 TEST(container_SmartPtr, Dynamic) {
     SmartPtr<ElementType, Dynamic> ptr = nullptr;
     EXPECT_FALSE(ptr);
+    EXPECT_TRUE(ptr.unique);
 
     ptr.reset(Construct, 42);
     EXPECT_TRUE(ptr);
+    EXPECT_TRUE(ptr.unique);
     EXPECT_EQ(ptr->getValue(), 42);
     EXPECT_EQ(ptr->getType(), ElementType::Init);
 
     SmartPtr<ElementType, Dynamic> copy(ptr);
     EXPECT_TRUE(copy);
+    EXPECT_FALSE(copy.unique);
+    EXPECT_FALSE(ptr.unique);
     EXPECT_EQ(&*copy, (ElementType *) ptr);
 
     {
@@ -56,14 +69,19 @@ TEST(container_SmartPtr, Dynamic) {
         EXPECT_TRUE(moved);
         EXPECT_TRUE(copy);
         EXPECT_FALSE(ptr);
+        EXPECT_FALSE(moved.unique);
+        EXPECT_FALSE(copy.unique);
+        EXPECT_TRUE(ptr.unique);
         EXPECT_EQ(&*copy, &*moved);
     }
 
     SmartPtr<ElementType, Dynamic> dummy(Construct);
     EXPECT_TRUE(dummy);
+    EXPECT_FALSE(dummy.unique);
     EXPECT_EQ(dummy->getValue(), 0);
     EXPECT_NE(&*dummy, &*copy);
     EXPECT_TRUE(copy);
+    EXPECT_FALSE(copy.unique);
     EXPECT_EQ(copy->getValue(), 42);
     EXPECT_EQ(copy->getType(), ElementType::Init);
 }
